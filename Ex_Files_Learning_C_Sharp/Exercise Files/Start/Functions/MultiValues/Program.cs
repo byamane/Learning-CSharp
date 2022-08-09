ad4d924ca597345569d2cf0ffde19413e4d8bd17
byamane <byamane21@gmail.com>
2022-08-09 12:31:09 -0500

    complete section

--- Ex_Files_Learning_C_Sharp/Exercise Files/Start/Functions/MultiValues/Program.cs
+++ Ex_Files_Learning_C_Sharp/Exercise Files/Start/Functions/MultiValues/Program.cs
@@ -6,17 +6,25 @@ namespace MultiValues
     {
         static void Main(string[] args)
         {
-            // TODO: Tuples are grouped values added in C# 7
+            // Tuples are grouped values added in C# 7
+            (int a, int b) tup1 = (5, 10);
+            var tup2 = ("Some text", 10.5);
 
+            // Tuple values are mutable
+            tup1.b = 20;
+            tup2.Item1 = "New String";
 
-            // TODO: Tuple values are mutable
+            Console.WriteLine($"{tup1.a},{tup1.b}");
+            Console.WriteLine($"{tup2.Item1},{tup2.Item2}");
 
-
-            // TODO: Functions can work with tuples
-
+            // Functions can work with tuples
+            (int, int) result = PlusTimes(6, 12);
+            Console.WriteLine($"Sum: {result.Item1}, Product: {result.Item2}");
         }
 
-        // TODO: Functions can return multiple values using tuples
-
+        // Functions can return multiple values using tuples
+        static (int, int) PlusTimes(int a, int b) {
+            return (a+b, a*b);
+        }
     }
 }
